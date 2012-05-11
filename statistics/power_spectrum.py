Fixed scaling in power spectrum

statistics/power_spectrum.py
@@ -15,6 +15,11 @@ def power_spectrum_nd(input_array):
 	power_spectrum = np.abs(ft)**2
 	utils.print_msg( '...done')
 
+	# scale
+	boxvol = conv.LB**len(input_array.shape)
+	pixelsize = boxvol/(np.product(input_array.shape))
+	power_spectrum *= pixelsize**2/boxvol
+
 	return power_spectrum
 
 def cross_power_spectrum_nd(input_array1, input_array2):
@@ -32,58 +37,12 @@ def cross_power_spectrum_nd(input_array1, input_array2):
 	return power_spectrum
 
 
-#def radial_average(input_array, dim=2, nbins=100):
-#	''' Take an n-dimensional powerspectrum and return the radially averaged 
-#	version
-#	Return P(k), k (Mpc^-1)'''
-#	#TODO: use middle of k bins instead of edges, scale Pk correctly
-#
-#	print 'WARNING: the power spectrum scaling is probably wrong...'
-#
-#	utils.print_msg('Radial average...')
-#
-#	if dim == 2:
-#		y,x = np.indices(input_array.shape)
-#		center = np.array([(x.max()-x.min())/2.0, (x.max()-x.min())/2.0])
-#		r = np.hypot(x - center[0], y - center[1])
-#	elif dim == 3:
-#		y,x,z = np.indices(input_array.shape)
-#		center = np.array([(x.max()-x.min())/2.0, (x.max()-x.min())/2.0, (z.max()-z.min())/2.0])
-#		r = np.sqrt((x - center[0])**2 + (y - center[1])**2 + (z-center[2])**2)
-#	else:
-#		raise Exception('Check your dimensions!')
-#
-#	dr = r.max()/float(nbins)
-#	radial = np.arange(r.max()/dr)*dr + dr/2.
-#	nrad = len(radial)
-#
-#	working_mask = np.ones(input_array.shape, bool)
-#	radialdata = np.zeros(nrad)
-#	
-#
-#	for irad in range(nrad): 
-#		percent_done = int(float(irad)/float(nrad)*100)
-#		if percent_done%10 == 0:
-#			utils.print_msg('%d %%' % percent_done)
-#
-#		minrad = irad*dr
-#		maxrad = minrad + dr
-#		thisindex = (r>=minrad) * (r<maxrad) * working_mask
-#		if not thisindex.ravel().any():
-#			radialdata[irad] = np.nan
-#		else:
-#			radialdata[irad] = input_array[thisindex].mean()
-#
-#	k = 2.*np.pi/conv.LB*radial
-#
-#	return radialdata, k
 
 
 def radial_average(input_array, dim=2, nbins=0):
 	''' Take an n-dimensional powerspectrum and return the radially averaged 
 	version. For internal use mostly.
 	Return P(k), k (Mpc^-1)'''
-	#TODO: use middle of k bins instead of edges, scale Pk correctly
 
 	if dim == 2:
 		y,x = np.indices(input_array.shape)
@@ -109,18 +68,15 @@ def radial_average(input_array, dim=2, nbins=0):
 	csum = np.cumsum(i_sorted, dtype='float64')	#Cumul. sum of sorted, flattened data
 	tbin = csum[r_ind[1:]] - csum[r_ind[:-1]]#part of the csum falling at a given radius
 
-	#k_min=2.*np.pi/conv.LB; #Mpc^-1
-	#k_max=k_min*(input_array.shape[0]/2.)*np.sqrt(3.);
-	
 	rvals = r_sorted[r_ind[1:]]
-	#k = (rvals-rvals.min())/rvals.max()*(k_max-k_min)+k_min
+	dr = rvals[1]-rvals[0]
 
-	k = 2.*np.pi/conv.LB*rvals
+	k = 2.*np.pi/conv.LB*(rvals-dr/2.)
 
 	return tbin.astype('float64')/nr.astype('float64'), k
 	
 
-def power_spectrum_1d(input_array_nd, bins=100):
+def power_spectrum_1d(input_array_nd, nbins=100):
 	''' Calculate the power spectrum of input_array_nd (2 or 3 dimensions)
 	and return it as a one-dimensional array 
 	Return P(k), k (in Mpc^-1)'''
@@ -128,12 +84,7 @@ def power_spectrum_1d(input_array_nd, bins=100):
 
 	input_array = power_spectrum_nd(input_array_nd)	
 
-	# scale
-	boxvol = conv.LB**len(input_array.shape)
-	pixelsize = boxvol/(np.product(input_array.shape))
-	input_array *= pixelsize/boxvol
-
-	return radial_average(input_array, dim=len(input_array_nd.shape), nbins=bins)
+	return radial_average(input_array, dim=len(input_array_nd.shape), nbins=nbins)
 
 def cross_power_spectrum1d(input_array1_nd, input_array2_nd):
 	''' Calculate the power spectrum of input_array_nd (2 or 3 dimensions)
@@ -144,8 +95,45 @@ def cross_power_spectrum1d(input_array1_nd, input_array2_nd):
 
 	return radial_average(input_array, dim=len(input_array1_nd.shape))
 
-def power_spectrum_mu(input_array, mubins):
-	pass
+def power_spectrum_mu(input_array, nbins=20):
+	'''
+	Calculate the power spectrum and bin it in mu=cos(theta)
+	'''
 
+	dim = len(input_array.shape)
+	if dim == 2:
+		y,x = np.indices(input_array.shape)
+		center = np.array([(x.max()-x.min())/2.0, (x.max()-x.min())/2.0])
+		r = np.hypot(x - center[0], y - center[1])
+	elif dim == 3:
+		y,x,z = np.indices(input_array.shape)
+		center = np.array([(x.max()-x.min())/2.0, (x.max()-x.min())/2.0, (z.max()-z.min())/2.0])
+		r = np.sqrt((x - center[0])**2 + (y - center[1])**2 + (z-center[2])**2)
+	else:
+		raise Exception('Check your dimensions!')
 
+	#Line-of-sight distance from center (assume los_axis = 0 for now)
+	los_dist = x-center[0]
+
+	#mu=cos(theta) = k_par/k
+	mu = np.abs(los_dist)/np.abs(r)
+	mu[np.where(r < 0.001)] = np.nan
+
+	#Calculate the power spectrum
+	powerspectrum = power_spectrum_nd(input_array)	
+	powerspectrum[tuple(np.array(input_array.shape)/2)] = 0.
+
+	#Bin the data
+	dmu = 1./float(nbins)
+	mubins = np.linspace(dmu/2., 1.-dmu/2, nbins)
+	mudata = np.zeros(nbins)
+	working_mask = np.ones(input_array.shape, bool)
+	for i in range(nbins):
+		mu_min = mubins[i]-dmu/2.
+		mu_max = mubins[i]+dmu/2.
+		idx = (mu >= mu_min) * (mu < mu_max) * working_mask
+		print mu_min, mu_max, len(idx), np.mean(powerspectrum[idx])
+		mudata[i] = np.mean(powerspectrum[idx])
+
+	return mudata, mubins
 