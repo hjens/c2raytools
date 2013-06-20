2cc27d04c9472213ffd42c236f82667da2493312
Hannes Jensen <hjens@astro.su.se>
2013-06-20 16:40:50 +0200

    Fixed doc strings for misc.py

--- src/c2raytools/misc/misc.py
+++ src/c2raytools/misc/misc.py
@@ -1,7 +1,21 @@
 import numpy as np
+
 def gauss_kern(size, sizey = None, sigma=1.0):
-	''' Return a normalized gaussian kernel. 
-	if sizey is not set, it will be the same as sizex '''
+	''' 
+	Generate a normalized gaussian kernel. 
+	
+	Parameters:
+		* size (int): Width of output array in pixels.
+		* sizey = None (int): Width along the x axis. If this
+			is set to None, sizey = size
+		* sigma = 1.0 (float): The sigma parameter for the Gaussian.
+		
+	Returns:
+		numpy array with the Gaussian. The dimensions will be
+		size x size or size x sizey depending on whether
+		sizey is set. The Gaussian is normalized so that its
+		integral is 1.	
+	'''
 
 	size = int(size/2)
 	if not sizey:
@@ -15,21 +29,26 @@ def gauss_kern(size, sizey = None, sigma=1.0):
 
 	return g/g.sum()
 
-def smooth(array, sigma):
-	''' Smooth the input array with a Gaussian.
-		array -- the array to smooth
-		sigma -- the width of the kernel
+def smooth(input_array, sigma):
+	''' 
+	Smooth the input array with a Gaussian kernel.
+	
+	Parameters:
+		* input_array (numpy array): the array to smooth
+		* sigma (float): the width of the kernel
 
-	Returns the smoothed array
+	Returns:
+		The smoothed array. A numpy array with the same
+		dimensions as the input.
 	'''
 	from scipy import signal
 
-	assert(array.shape[0] == array.shape[1])
+	assert(input_array.shape[0] == input_array.shape[1])
 
 
-	kernel = gauss_kern(array.shape[0], sigma=sigma)
+	kernel = gauss_kern(input_array.shape[0], sigma=sigma)
 	
-	out =  signal.fftconvolve(array, kernel)
+	out =  signal.fftconvolve(input_array, kernel)
 
 	ox = out.shape[0]
 
