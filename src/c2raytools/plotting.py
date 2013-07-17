d8051cb6ba793806a9cb9638642545651a7b13d1
Hannes Jensen <hjens@astro.su.se>
2013-07-17 16:34:05 +0200

    Added histogram plot

--- src/c2raytools/plotting.py
+++ src/c2raytools/plotting.py
@@ -6,7 +6,8 @@ from helper_functions import get_data_and_type
 
 def plot_slice(data, los_axis = 0, slice_num = 0, logscale = False, **kwargs):
     '''
-    Plot a slice through a data cube.
+    Plot a slice through a data cube. This function will produce a nicely
+    formatted image plot with the correct units on the axes.
     
     Parameters:
         * data (XfracFile, DensityFile, string or numpy array): the data to 
@@ -23,6 +24,10 @@ def plot_slice(data, los_axis = 0, slice_num = 0, logscale = False, **kwargs):
         
     Returns:
         Nothing.
+        
+    Example (plot an xfrac file with a custom color map):
+        >>> xfile = c2t.XfracFile('xfrac3d_8.515.bin')
+        >>> c2t.plot_slice(xfile, cmap = pl.cmap.hot)
     '''
     
     import pylab as pl
@@ -50,30 +55,69 @@ def plot_slice(data, los_axis = 0, slice_num = 0, logscale = False, **kwargs):
     pl.xlabel('$\mathrm{cMpc}$')
     pl.ylabel('$\mathrm{cMpc}$')
     
+    #Make redshift string
+    try:
+        z_str = '$z = %.2f$' % data.z
+    except Exception:
+        z_str = ''
+    
     #Set labels etc
     if datatype == 'xfrac':
         if (logscale):
             cbar.set_label('$\log_{10} x_i$')
         else:
             cbar.set_label('$x_i$')
-        pl.title('Ionized fraction')
+        pl.title('Ionized fraction, %s' % z_str)
     elif datatype == 'density':
-        pl.title('Density')
         if (logscale):
             cbar.set_label('$\log_{10} \\rho \; \mathrm{[g \; cm^{-3}]}$')
         else:
             cbar.set_label('$\\rho \; \mathrm{[g \; cm^{-3}]}$')
+        pl.title('Density, %s' % z_str)
         
+def plot_hist(data, logscale = False, **kwargs):
+    '''
+    Plot a histogram of the data in a data cube.
+    
+    Parameters:
+        * data (XfracFile, DensityFile, string or numpy array): the data to 
+            plot. The function will try to determine what type of data it's 
+            been given. 
+        * logscale = False (bool): whether to plot the logarithm of the data
+            
+    Kwargs:
+        All kwargs are sent to matplotlib's hist function. Here, you can specify,
+        for example, the bins keyword
         
-if __name__ == '__main__':
-    import c2raytools as c2t
+    Returns:
+        Nothing.
+    '''
+
     import pylab as pl
     
-    dfilename = '/disk/sn-12/garrelt/Science/Simulations/Reionization/C2Ray_WMAP5/114Mpc_WMAP5/coarser_densities/nc256_halos_removed/6.905n_all.dat'
-    #plot_slice('/disk/sn-12/garrelt/Science/Simulations/Reionization/C2Ray_WMAP5/114Mpc_WMAP5/114Mpc_f2_10S_256/results_ranger/xfrac3d_8.958.bin',)
+    #Determine data type
+    plot_data, datatype = get_data_and_type(data)
     
-    dfile = c2t.DensityFile(dfilename)
-    plot_slice(dfile, los_axis=1, cmap=pl.cm.hot)
+    #Fix bins
+    if datatype == 'xfrac' and not 'bins' in kwargs.keys():
+        kwargs['bins'] = np.linspace(0,1,30)
+    else: 
+        kwargs['bins'] = 30
+        
+    #Plot
+    if not 'histtype' in kwargs.keys():
+        kwargs['histtype'] = 'step'
+    if not 'color' in kwargs.keys():
+        kwargs['color'] = 'k'
+    
+    pl.hist(plot_data.flatten(), log = logscale, **kwargs)
+        
+    #Labels
+    if datatype == 'xfrac':
+        pl.xlabel('$x_i$')
+    elif datatype == 'density':
+        pl.xlabel('$\\rho \; \mathrm{[g \; cm^{-3}]}$')
+        
     
     
     
