8c8f9777e0abeb1aacdc384cafcdd4c8714d237f
Garrelt Mellema <garrelt@astro.su.se>
2017-05-19 15:13:29 +0200

    Added the kurtosis function, from Hannah Ross's version.

--- src/c2raytools/statistics.py
+++ src/c2raytools/statistics.py
@@ -22,6 +22,25 @@ def skewness(x):
 	#return (sum(xdiff**3)/n)/((sum(xdiff**2)/n)**(3./2.)) #This is how SciPy does it
 	return (np.sum(xdiff**3)/n)/((np.sum(xdiff**2)/(n-1))**(3./2.))
 
+def kurtosis(x):
+        ''' 
+        Calculate the skewness of an array.
+        Note that IDL calculates the skewness in a slightly different way than Python. 
+        This routine uses the IDL definition. 
+        
+        Parameters:
+                * x (numpy array): The array containing the input data
+                
+        Returns:
+                The skewness.
+
+        '''
+        mx = np.mean(x)
+        n = np.size(x)
+        xdiff = x-mx
+        #return (sum(xdiff**3)/n)/((sum(xdiff**2)/n)**(3./2.)) #This is how SciPy does it
+        return (np.sum(xdiff**4)/n)/((np.sum(xdiff**2)/(n-1))**(2.))
+
 
 def mass_weighted_mean_xi(xi, rho):
 	''' Calculate the mass-weighted mean ionization fraction.
