29233df396a32abc5f40d18995afd32d8580388a
Garrelt Mellema <garrelt@astro.su.se>
2017-05-19 15:19:25 +0200

    Added functions for calculations of the diff. brightness temperature including spin temperature values. These should be checked, also because of the resolution fixes.

--- src/c2raytools/temperature.py
+++ src/c2raytools/temperature.py
@@ -35,7 +35,44 @@ def calc_dt(xfrac, dens, z = -1):
 	
 	#Calculate dT
 	return _dt(rho, xi, z)
+
+def calc_dt_full(xfrac, temp, dens, z = -1):
+	'''
+	Calculate the differential brightness temperature assuming only that Lyman alpha is fully coupled so T_s = T_k
+    (NOT T_s >> T_CMB)
 	
+	Parameters:
+		* xfrac (XfracFile object, string or numpy array): the ionization fraction
+        * temp (TemperFile object, string or numpy array): the temperature in K
+		* dens (DensityFile object, string or numpy array): density in cgs units
+		* z = -1 (float): The redshift (if < 0 this will be figured out from the files)
+		
+	Returns:
+		The differential brightness temperature as a numpy array with
+		the same dimensions as xfrac.
+	'''
+
+	xi, xi_type = get_data_and_type(xfrac)
+        Ts, Ts_type = get_data_and_type(temp)
+	rho, rho_type = get_data_and_type(dens)
+	xi = xi.astype('float64')
+        Ts = Ts.astype('float64')
+	rho = rho.astype('float64')
+	
+	if z < 0:
+		z = determine_redshift_from_filename(xfrac)
+		if z < 0:
+			z = determine_redshift_from_filename(dens)
+                        if z < 0:
+                		z=determine_redshift_from_filename(temp)
+				if z < 0:
+					raise Exception('No redshift specified. Could not determine from file.')
+	
+	print_msg('Making full dT box for z=%f' % z)
+	
+	#Calculate dT
+        print "calculating corrected dbt"
+	return _dt_full_corrected(dens,xfrac,temp,z)#rho, Ts, xi, z)
 
 def calc_dt_lightcone(xfrac, dens, lowest_z, los_axis = 2):
 	'''
@@ -71,6 +108,46 @@ def calc_dt_lightcone(xfrac, dens, lowest_z, los_axis = 2):
 	z = cosmology.cdist_to_z(cdist)
 	return _dt(dens, xfrac, z)
 
+def calc_dt_full_lightcone(xfrac, temp, dens, lowest_z, los_axis = 2):
+	'''
+	Calculate the differential brightness temperature assuming only that Lyman alpha is fully coupled so T_s = T_k
+    (NOT T_s >> T_CMB) for lightcone data. UNTESTED
+	
+	Parameters:
+		* xfrac (string or numpy array): the name of the ionization 
+			fraction file (must be cbin), or the xfrac lightcone data
+        * temp (string or numpy array): the name of the temperature
+            file (must be cbin), or the temp lightcone data
+		* dens (string or numpy array): the name of the density 
+			file (must be cbin), or the density data
+		* lowest_z (float): the lowest redshift of the lightcone volume
+		* los_axis = 2 (int): the line-of-sight axis
+		
+	Returns:
+		The differential brightness temperature as a numpy array with
+		the same dimensions as xfrac.
+	'''
+	
+	try:
+		xfrac = read_cbin(xfrac)
+	except Exception:
+		pass
+        try:
+		temp = read_cbin(temp)
+	except Exception:
+		pass
+	try:
+		dens = read_cbin(dens)
+	except:
+		pass
+	dens = dens.astype('float64')
+		
+	cell_size = conv.LB/xfrac.shape[(los_axis+1)%3]
+	cdist_low = cosmology.z_to_cdist(lowest_z)
+	cdist = np.arange(xfrac.shape[los_axis])*cell_size + cdist_low
+	z = cosmology.cdist_to_z(cdist)
+        print "Redshift: ", str(z)
+	return _dt_full_corrected(dens, xfrac,temp, z)
 
 def mean_dt(z):
 	'''
@@ -99,5 +176,49 @@ def _dt(rho, xi, z):
 	dt = Cdt*(1.0-xi)*rho/rho_mean
 	
 	return dt
+
+def _dt_full(rho, xi, Ts, z):
+        print "Redshift: " +str(z) +'\n'
+	rho_mean = const.rho_crit_0*const.OmegaB
+        Tcmb = 2.725*(1+z) # might want to add to cosmology.py instead
+	Cdt = mean_dt(z)
+        print type(Ts),type(Tcmb),type(Cdt),type(xi),type(rho)
+        dt = ((Ts.temper-Tcmb)/Ts.temper)*Cdt*(1.0-xi)*rho/rho_mean  #extra term for temperature fluctuations as Ts is not much greater than Tcmb, Hannah Ross
+        #dt = ((Ts-Tcmb)/Ts)*Cdt*(1.0-xi.xi)*rho.cgs_density/rho_mean  #extra term for temperature fluctuations as Ts is not much greater than Tcmb, Hannah Ross
+#	dt = (1+Tcmb/Ts)*Cdt*(1.0-xi)*rho/rho_mean
 	
+	return dt
+	
+def _dt_full_corrected(rho, xi, Ts, z):
+        z = np.mean(z)
+        print "Redshift: " +str(z) +'\n'
+        rho_mean = const.rho_crit_0*const.OmegaB
+        Tcmb = 2.725*(1+z) # might want to add to cosmology.py instead
+        Cdt = mean_dt(z)
+
+        zi = 1089
+        Tcmb0=2.726
+        Ti=Tcmb0*(1+zi)
+        T_HII=2.0e4
+        T_min = Ti*(1+z)**2/(1+zi)**2
+#        print T_min
+        ones=np.ones(250**3,dtype=float).reshape(250,250,250)
+#        print z
+        #print type(z),type(Ts.temper), type(T_min), type(T_HII), type(xi)
+        for i in range(len(xi[:,1,1])):
+            for j in range(len(xi[1,:,1])):
+                for k in range(len(xi[1,1,:])):
+                    Ts.temper[i,j,k] = max(T_min,(Ts.temper[i,j,k]-xi[i,j,k]*T_HII)/(1.-xi[i,j,k]))
+                        
+ #       print np.mean(Ts.temper)
+#        Ts.temper = (Ts.temper-xi*T_HII)/(ones-xi)
+        if np.any(Ts.temper < 0):
+            print "WARNING: negative temperatures"
+#        print type(Ts),type(Tcmb),type(Cdt),type(xi),type(rho)
+
+        dt = ((Ts.temper-Tcmb)/Ts.temper)*Cdt*(1.0-xi)*rho/rho_mean  #extra term for temperature fluctuations as Ts is not much greater than Tcmb, Hannah Ross
+        #dt = ((Ts-Tcmb)/Ts)*Cdt*(1.0-xi.xi)*rho.cgs_density/rho_mean  #extra term for temperature fluctuations as Ts is not much greater than Tcmb, Hannah Ross
+#       dt = (1+Tcmb/Ts)*Cdt*(1.0-xi)*rho/rho_mean
+
+        return dt
 
